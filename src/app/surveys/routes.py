e695b3fadfa90242036b9259da686f4ebbbb4886
Ezequiel Vega <vegaezequiel51@gmail.com>
2020-09-28 01:11:56 -0300

    Se agrego la agrupacion de etiquetas

--- src/app/surveys/routes.py
+++ src/app/surveys/routes.py
@@ -65,3 +65,63 @@ def one_survery():
          answers.append(answer_dic)
 
    return jsonify({'survery': survery, 'answers': answers})
+
+@app.route('/api/v1/survery/labels')
+def labels_survery():
+   # Obtener etiquetas
+   labels = request.json['labels']
+
+   # Obtener todas las encuestas
+   surverys_db = Surverys.get_survery_all()
+   
+   # Crear variable de las encuestas a mostrar
+   surverys = list()
+   
+   # Recorer lista obtenida en la base de datos
+   for survery in surverys_db:
+      if len(labels) == 1:
+         if labels[0] in survery.labels:
+            # Estructurar datos a mostrar
+            survery_dict = {
+               'id': survery.id,
+               'name_survery': survery.name_survery,
+               'questions': survery.questions,
+               'deadline': survery.deadline,
+               'created_date': survery.created_date,
+               'labels': survery.labels
+            }
+
+            # Guardar los datos en la lista de las encuestas
+            surverys.append(survery_dict)
+      elif len(labels) >= 2:
+         for label in  labels:
+            if label in survery.labels:
+               # Estructurar datos a mostrar
+               survery_dict = {
+                  'id': survery.id,
+                  'name_survery': survery.name_survery,
+                  'questions': survery.questions,
+                  'deadline': survery.deadline,
+                  'created_date': survery.created_date,
+                  'labels': survery.labels
+               }
+
+               # Verifico que no se repita los datos
+               if survery_dict not in surverys:
+                  # Guardar los datos en la lista de las encuestas
+                  surverys.append(survery_dict)
+      else:
+         # Estructurar datos a mostrar
+         survery_dict = {
+            'id': survery.id,
+            'name_survery': survery.name_survery,
+            'questions': survery.questions,
+            'deadline': survery.deadline,
+            'created_date': survery.created_date,
+            'labels': survery.labels
+         }
+
+         # Guardar los datos en la lista de las encuestas
+         surverys.append(survery_dict)
+
+   return jsonify({'surverys': surverys})
