e9b5ee0350dbf9bf1ca6ddc5260f4166c0500161
Ezequiel vega <39246608+Ezequiel-Vega@users.noreply.github.com>
2020-09-29 14:28:00 -0300

    Se elimino el admin

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -19,12 +19,10 @@ def config(app: Flask):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 def routes(app: Flask):
-    from .admin import admin_bp
     from .auth import auth_bp
     from .surveys import survery_bp
     from .user import user_bp
 
-    app.register_blueprint(admin_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(survery_bp)
     app.register_blueprint(user_bp)
